63ae243c9f8bf4a85bb7a9356df259855b1852fa
Alexandre Lefebvre <yeakoo@outlook.com>
2018-03-13 12:04:05 +0000

    Adding Challenge 1 source code

---
+++ Challenge-1/bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`I'm fucking ready... name is ${client.user.username}`);
+});
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === '--bot') msg.channel.send(`Yes ${msg.author.username}?`);
+  if(msg.content.toLowerCase() === '--botmention') msg.channel.send(`Yes ${msg.author.toString()}?`);
+});
+client.login('NDA5MTMwOTU2NDk5MTg5Nzgx.DYkNGw.1Xl7qu8ibTe9oO5CewqWKipx658');
